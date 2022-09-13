5797d00c49f7b2d6733c1d244d85cb909e4fa30c
prog_dsp <saw.coding@gmail.com>
2022-09-13 16:37:57 +0300

    add kata 2

--- kata1.js
+++ kata1.js
@@ -14,3 +14,10 @@ function index(array, n){
     return -1
   }
 }
+
+// другие решения:
+// const index = (array, n) => array.length > n ? Math.pow(array[n], n) : -1;
+
+// function index(array, n){
+//   return array[n] ** n || -1;
+// }
