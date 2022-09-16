594d2665b40d550d3ae446207fa1b5d53af316df
prog_dsp <saw.coding@gmail.com>
2022-09-16 13:19:18 +0300

    Abbreviate a Two Word Name

--- kata13.js
+++ kata13.js
@@ -1,2 +1,22 @@
+// Abbreviate a Two Word Name
 
-Abbreviate a Two Word Name
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+  return name.match(/\b\w/g).join('.').toUpperCase()
+}
+
+// другое решение:
+// function abbrevName(name){
+
+//   var nameArray = name.split(" ");
+//   return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+// }
