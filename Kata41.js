658908e5ac24a84c2a49debf5dda821e72f482fe
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-07-15 08:12:57 +0300

    kata41

---
+++ Kata41.js
@@ -0,0 +1,18 @@
+// You woke up from a massive headache and can't seem to find your car key. You find a note with a clue that says:
+//
+// "If you're reading this then I have left the state and am well on my way to freedom. Just to make things interesting, I have also provided something for you to track me with. Let the chase begin..."
+//
+// Given an array of binary numbers, figure out and return the culprit's message to find out who took the missing car key.
+//
+// > whoTookTheCarKey(['01000001', '01101100', '01100101', '01111000', '01100001', '01101110', '01100100', '01100101', '01110010'])
+// 'Alexander'
+
+function whoTookTheCarKey(message) {
+
+  let binCode = [];
+
+  for (i = 0; i < message.length; i++) {
+    binCode.push(String.fromCharCode(parseInt(message[i], 2)));
+  }
+  return binCode.join("");
+}
